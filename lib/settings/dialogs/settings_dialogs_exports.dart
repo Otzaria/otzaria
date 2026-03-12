7961f67dcdefbeed1c2e452181c6a2fd977ffc96
DeveShlomo <sz115682@gmaol.com>
2026-03-12 23:03:35 +0200

    מחיקת קבצי דיאלוג מיותרים

--- lib/settings/dialogs/settings_dialogs_exports.dart
+++ lib/settings/dialogs/settings_dialogs_exports.dart
@@ -1,5 +1,3 @@
 // Barrel export for all settings dialogs
-export 'calendar_settings_dialog.dart';
-export 'gematria_settings_dialog.dart';
 export 'library_settings_dialog.dart';
 export 'reading_settings_dialog.dart';
