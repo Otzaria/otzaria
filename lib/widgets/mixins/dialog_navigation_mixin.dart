4c7dcfa53eb9cde740e154a8b326dad62ac6fb4b
DeveShlomo <sz115682@gmaol.com>
2026-03-26 18:17:03 +0200

    שינוי שם לפוקוס בדיאלוגים

--- lib/widgets/mixins/dialog_navigation_mixin.dart
+++ lib/widgets/mixins/dialog_navigation_mixin.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:otzaria/widgets/dialog_keyboard_navigator.dart';
+import 'package:otzaria/widgets/keyboard_dialog_navigation.dart';
 
 mixin DialogNavigationMixin<T extends StatefulWidget> on State<T> {
   int focusedButtonIndex = 1; // 0 = Cancel, 1 = Confirm
