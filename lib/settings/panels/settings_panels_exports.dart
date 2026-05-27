da97b701b57fe922e3d6716db2117af1893923b6
DeveShlomo <sz115682@gmaol.com>
2026-05-27 20:16:03 +0300

    עריכה: מחיקת קובץ מיותר

--- lib/settings/panels/settings_panels_exports.dart
+++ lib/settings/panels/settings_panels_exports.dart
@@ -1,6 +1,5 @@
 // Barrel export for all settings panels
 export 'calendar_settings_panel.dart';
 export 'gematria_settings_panel.dart';
-export 'editor_settings_panel.dart';
 export 'reading_settings_panel.dart';
 export 'library_settings_panel.dart';
