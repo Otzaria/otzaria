78850739b3d5a656bf67baa996f4f0245369f60e
Y-PLONI <7353755@gmail.com>
2026-07-25 22:52:57 +0300

    תיקון הערות סקירה
    תיקון לקומיט 21281d3b4

--- lib/text_book/view/text_book_search_screen.dart
+++ lib/text_book/view/text_book_search_screen.dart
@@ -352,6 +352,7 @@ class TextBookSearchViewState extends State<TextBookSearchView>
           setState(() {
             searchResults = [];
             _isSearching = false;
+            _searchErrorMessage = TextBookMessages.searchContentLoadFailed;
           });
         }
         return;
